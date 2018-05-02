a0dd6e23a026f6d8d0c46b8e233893e66422b89f
Sushil K Bhaskar <Sushil K Bhaskar@OptimusPrime>
2018-05-02 12:49:32 +0530

    log 4j removed

--- SpringSecurity/src/main/java/com/skbh/robot/HomeController.java
+++ SpringSecurity/src/main/java/com/skbh/robot/HomeController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -30,7 +29,7 @@ import com.skbh.robot.model.LoginDetails;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = Logger.getLogger(HomeController.class);
+	//private static final Logger logger = Logger.getLogger(HomeController.class);
 	private String className;
 	
 	
@@ -46,7 +45,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}." +  locale);
+		//logger.info("Welcome home! The client locale is {}." +  locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
